0d1bba0df1573b6ef903d64d5d5f8b56be96cad5
tanjid.du <tanjid.du@gmail.com>
2022-08-23 19:06:48 +0600

    commit message

--- myoras/settings.py
+++ myoras/settings.py
@@ -146,7 +146,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
